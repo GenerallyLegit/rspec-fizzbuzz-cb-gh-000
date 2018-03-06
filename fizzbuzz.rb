7096c1a0f07f31d52fd4580da84d1c31f090e937
Niall McPaul <niall.mcpaul@queens.ox.ac.uk>
2018-03-06 20:26:21 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,4 +1,11 @@
 def fizzbuzz(num)
   if num % 3 == 0
     puts "Fizz"
+  elsif num % 5 == 0
+    puts "Buzz"
+  elsif num % 5 == 0 && num % 3 == 0
+    puts "FizzBuzz"
+  else
+    return nil
   end
+end
